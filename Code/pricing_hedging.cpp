4f60ae1854470413d3a3bbfc5f69ff013001419b
maximelefebvre <maxime.lefebvre59139@gmail.com>
2017-12-28 19:57:51 -0500

    Add data

--- Code/pricing_hedging.cpp
+++ Code/pricing_hedging.cpp
@@ -14,7 +14,7 @@
 namespace model
 {
     EuropeanOption::EuropeanOption(TypeOption typeoption, Spot* spot, Strikes* strikes, InterestRate* ir, DividendYield* dy, Volatility* volatility, Maturity* maturity)
-    : m_TypeOption(typeoption), 
+    : m_TypeOption(typeoption), p_Spot(spot), p_Strikes(strikes), p_IR(ir), p_DY(dy), p_Volatility(volatility), p_Maturity(maturity)
     {
         std::cout << "Construction of European Option" << std::endl;
     }

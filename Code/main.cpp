9041f027d55fe90782fcc0b618a7ccdfff77a58a
siou0107 <louis.lepasdesecheval@gmail.com>
2018-01-06 16:38:23 +0100

    pricing_hedging: includes option value at maturity, when d1 and d2 -> ± ∞; main: modified to allow for Start- and EndDate parameters

--- Code/main.cpp
+++ Code/main.cpp
@@ -13,41 +13,64 @@
 #include "import_data.hpp"
 #include "parameters.hpp"
 #include "pricing_hedging.hpp"
+#include "dates.hpp"
 
-double ValueOfPnL(const double volatility, const double strikeprice, const double maturity, const std::vector<double> Spots)
+//double ValueOfPnL(const double volatility, const double strikeprice, const double maturity, const std::vector<double> Spots)
+double ValueOfPnL(const double volatility, const double strikeprice, const date StartDate, const date EndDate, const std::vector<double> Spots)
 {
-    std::size_t LengthVector = Spots.size();
+    //std::size_t LengthVector = Spots.size();
+    NbDays = EndDate - StartDate;
     
     model_params::InterestRate ir(0.1);
     model_params::DividendYield dy(0.02);
-    model_params::Spot spot(100.0);
+    //model_params::Spot spot(100.0);
+    model_params::Spot spot(Spots[0]);
     model_params::Strike strike(strikeprice);
     model_params::Volatility vol(volatility);
-    model_params::Maturity mat(maturity);
+    //model_params::Maturity mat(maturity);
+    model_params::Maturity mat(double(NbDays)/365.0);
     
-    model::EuropeanOption option("Call", spot, strike, ir, dy, vol, mat);
-    std::cout << "Price: " << option.Price() << std::endl;
-    std::cout << "Delta: " << option.Delta() << std::endl;
-    
-    std::vector<double> Prices(LengthVector,0.0);
+    /*std::vector<double> Prices(LengthVector,0.0);
     std::vector<double> Delta(LengthVector,0.0);
     std::vector<double> SpotPositions(LengthVector,0.0);
     std::vector<double> CashPositions(LengthVector,0.0);
-    std::vector<double> PnLVector(LengthVector,0.0);
+    std::vector<double> PnLVector(LengthVector,0.0);*/
     
-    for(std::size_t i = 0;i<LengthVector;++i)
+    std::vector<double> Prices(NbDays+1,0.0);
+    std::vector<double> Delta(NbDays+1,0.0);
+    std::vector<double> SpotPositions(NbDays+1,0.0);
+    std::vector<double> CashPositions(NbDays+1,0.0);
+    std::vector<double> PnLVector(NbDays+1,0.0);
+    
+    model::EuropeanOption option("Call", spot, strike, ir, dy, vol, mat);
+    Prices[0] = option.Price();
+    Delta[0] = option.Delta();
+    std::cout << "Price: " << option.Price() << std::endl;
+    std::cout << "Delta: " << option.Delta() << std::endl;
+    SpotPositions[0]=option.UnderlyingH();
+    CashPositions[0]=option.Cash();
+    PnLVector[0]=0.0
+    
+    for(std::size_t i = 1;i<NbDays+1;++i)
     {
-        model_params::Maturity mat2(maturity*(1.0-i/LengthVector));
+        NbDaysLeft = EndDate - StartDate + i
+        /*model_params::Maturity mat2(double(NbDaysLeft)/365.0);
         model::EuropeanOption option2("Call", Spots[i], strike, ir, dy, vol, mat2);
         Prices[i] = option2.Price();
         Delta[i] = option2.Delta();
         SpotPositions[i] = option2.UnderlyingH();
-        CashPositions[i] = option2.Cash();
-        
-        if(i > 0)
+        CashPositions[i] = option2.Cash();*/
+        model_params::Maturity mat(double(NbDaysLeft)/365.0);
+        model::EuropeanOption option("Call", Spots[i], strike, ir, dy, vol, mat);
+        Prices[i] = option.Price();
+        Delta[i] = option.Delta();
+        SpotPositions[i] = option.UnderlyingH();
+        CashPositions[i] = option.Cash();
+        PnLVector[i] = (Prices[i] - Prices[i-1]) - Delta[i-1]*(Spots[i]-Spots[i-1]) - std::exp(ir.value() *maturity/LengthVector)*CashPositions[i-1];
+        /*if(i > 0)
         {
             PnLVector[i] = (Prices[i] - Prices[i-1]) - Delta[i-1]*(Spots[i]-Spots[i-1]) - std::exp(ir.value() *maturity/LengthVector)*CashPositions[i-1];
-        }
+        }*/
     }
     double PnL = std::accumulate(PnLVector.begin(), PnLVector.end(), 0);
     return PnL;
@@ -58,9 +81,9 @@ double FindVolatility(const double strikeprice, const double maturity, const std
     double LowerBound = 0.;
     double UpperBound = 25.;
     double temp = (LowerBound+UpperBound)/2;
-    while(std::abs(ValueOfPnL(temp,strikeprice,maturity,Spots)) > std::pow(10,-3))
+    while(std::abs(ValueOfPnL(temp/100,strikeprice,maturity,Spots)) > std::pow(10,-3))
     {
-        if(ValueOfPnL(temp,strikeprice,maturity,Spots) >= 0)
+        if(ValueOfPnL(temp/100,strikeprice,maturity,Spots) >= 0)
         {
             LowerBound = temp;
         }
--- Code/pricing_hedging.cpp
+++ Code/pricing_hedging.cpp
@@ -33,6 +33,8 @@ namespace model
             z = -1.0;
         }
         
+        if m_Maturity = 0.0 return std::max(z*(m_Spot.value()-m_Strike.value()),0.0)
+        
         double d1 = (std::log(m_Spot.value()/m_Strike.value()) + (m_IR.value() - m_DY.value() + 0.5*m_Volatility.value() * m_Volatility.value())*m_Maturity.value()) / (m_Volatility.value() * std::sqrt(m_Maturity.value()));
         double d2 = d1 - m_Volatility.value() * std::sqrt(m_Maturity.value());
         return z*(m_Spot.value()*std::exp(-m_DY.value()*m_Maturity.value())*func::Gaussian_Distrib_CDF(z*d1) - m_Strike.value()*std::exp(-m_IR.value()*m_Maturity.value())*func::Gaussian_Distrib_CDF(z*d2));
@@ -49,6 +51,12 @@ namespace model
         else
         {
             z = -1.0;
+        }
+        
+        if m_Maturity = 0.0
+        {
+            if std::max(z*(m_Spot.value()-m_Strike.value()),0.0) = 0 return 0;
+            else return z
         }
         
         double d1 = (std::log(m_Spot.value()/m_Strike.value()) + (m_IR.value() - m_DY.value() + 0.5*m_Volatility.value()*m_Volatility.value())*m_Maturity.value()) / (m_Volatility.value() * std::sqrt(m_Maturity.value()));
